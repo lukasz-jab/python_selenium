db6a2a8e5ccbf2e5c05618765919a22e09371589
Łukasz Jabłoński <lukasz.j.pl@interia.pl>
2021-02-15 19:01:32 +0100

    Optimized index of contact for test function

--- src/test/test_data_between_home_and_edit.py
+++ src/test/test_data_between_home_and_edit.py
@@ -1,10 +1,15 @@
 import random
 import re
 
+from src.model.contact import Contact
+
 
 def test_data_home_edit_sites(app):
-    index = app.contact.get_contacs_list()
-    contact_from_home_page = app.contact.get_contacs_list()[random.randint(0,len(index)-1)]
+    if app.contact.count() == 0:
+        app.contact.create(
+            Contact("Precond name", "Precond last", "Precon address", "00000", " Precond notes notes notes"))
+    index = app.contact.count()
+    contact_from_home_page = app.contact.get_contacs_list()[random.randint(0,index-1)]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(contact_from_home_page.id)
     # test all phones in file: test_phones.py
     assert contact_from_home_page.all_emails == merge_emails_like_on_home_page(contact_from_edit_page)
