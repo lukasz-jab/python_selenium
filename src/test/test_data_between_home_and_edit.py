dda6edffcf60d373c17c0d3e74c12357c9b79c59
Łukasz Jabłoński <lukasz.j.pl@interia.pl>
2021-02-20 16:26:56 +0100

    Combined tests from test_phones.py into test_data_between_home_and_edit.py

--- src/test/test_data_between_home_and_edit.py
+++ src/test/test_data_between_home_and_edit.py
@@ -14,7 +14,7 @@ def test_data_home_edit_sites(app):
     # test all phones in file: test_phones.py
     assert contact_from_home_page.all_emails == merge_emails_like_on_home_page(contact_from_edit_page)
     # main page cut more than one space " "
-    assert clear(contact_from_home_page.address) == clear(contact_from_edit_page.address)
+    assert clear_h(contact_from_home_page.address) == clear_h(contact_from_edit_page.address)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
 
@@ -22,5 +22,30 @@ def test_data_home_edit_sites(app):
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "", (map (lambda x: x, (filter(lambda x: x is not None,
                                                            [contact.emial_1, contact.emial_2, contact.emial_3]))))))
-def clear(s):
+def clear_h(s):
     return re.sub(" ", "", s)
+
+def test_phones_on_main_page(app):
+    contact_from_home_page = app.contact.get_contacs_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(contact_from_home_page.id)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    # in aplication version 9.0, secondaryphone(fax) is not displayed in home page in the table
+
+
+def test_phones_on_view_page(app):
+    test_contact = app.contact.get_contacs_list()[0]
+    contact_from_view_page = app.contact.get_contact_info_from_view_page(test_contact.id)
+    print(contact_from_view_page.all_phones_from_home_page)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(test_contact.id)
+    assert clear(contact_from_view_page.all_phones_from_home_page) == merge_phones_like_on_home_page(contact_from_edit_page)
+    # in aplication version 9.0, secondaryphone is field fax not added due to incompatibility with home page test
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            (map(lambda x: clear(x), filter(lambda x: x is not None,
+                                                            [contact.homephone, contact.mobilephone, contact.workphone])))))
